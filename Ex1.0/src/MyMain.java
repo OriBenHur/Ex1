b12d50c30ca85503340e169709d4d0e9d7558384
MaayanBH <maayan008@gmail.com>
2016-03-29 18:01:05 +0300

    Junit

--- Ex1.0/src/MyMain.java
+++ Ex1.0/src/MyMain.java
@@ -7,55 +7,9 @@ import java.util.Date;
 
 public class MyMain
 {
-	public static void main(String[] args) //throws Exception
+
+	public static void main(String[] args)
 	{
-		try
-		{
-			long start = new Date().getTime();
-			String time = new SimpleDateFormat("HH.mm.ss").format(start);
-			System.out.println("Start Testing...");
-			System.out.println("Loading graph file: " + args[0] + " runing a test " + args[1]);
-			String ans = "Solution_" + args[1] + "_" + args[0] + "_" + time + ".txt";
-
-			FileWriter fw = new FileWriter(ans);
-			PrintWriter os = new PrintWriter(fw);
-			In in = new In(args[0]);
-			EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
-			long t1 = new Date().getTime();
-			System.out.println("Done loading Graph: " + args[0] + "  in " + (t1 - start) + "  ms");
-			FileReader fr = new FileReader(args[1]);
-			BufferedReader br = new BufferedReader(fr);
-			String num_of_lines = br.readLine();
-			String s = br.readLine();
-			int ll = 0;
-			os.println(num_of_lines + " Results:");
-			while ((s != null) && (ll < 20))
-			{
-				String[] sa = s.split(" ");
-				int source = Integer.parseInt(sa[0]);
-				int target = Integer.parseInt(sa[1]);
-				int size_of_BL = Integer.parseInt(sa[2]);
-				int[] BL = new int[size_of_BL];
-				for (int i = 0; i < size_of_BL; i++) 
-				{
-					BL[i] = Integer.parseInt(sa[(i + 3)]);
-				}
-				double dist = DijkstraSP.sp(G, source, target, BL);
-				os.println(s + " " + dist);
-				ll++;
-				s = br.readLine();
-			}
-			long t2 = new Date().getTime();
-			System.out.println("Done computing shortest paths on Graph: " + args[0] + "  in " + (t2 - t1) + "  ms");
-			System.out.println("Total time: " + (t2 - start) + "  ms");
-			os.close();
-			br.close();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
+		Helper.run(args[0], args[1]);
 	}
-
-
 }
